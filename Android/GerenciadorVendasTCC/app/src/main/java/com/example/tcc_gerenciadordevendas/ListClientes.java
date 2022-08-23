d038b7477d5fd3f6b63464c7bb6f23ae931c1f64
JohnAndante <walker_contas@hotmail.com>
2022-08-22 23:42:55 -0300

    Update ListClientes.java

--- Android/GerenciadorVendasTCC/app/src/main/java/com/example/tcc_gerenciadordevendas/ListClientes.java
+++ Android/GerenciadorVendasTCC/app/src/main/java/com/example/tcc_gerenciadordevendas/ListClientes.java
@@ -3,7 +3,8 @@ package com.example.tcc_gerenciadordevendas;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -26,17 +27,51 @@ public class ListClientes extends AppCompatActivity {
     private LinearLayout llAdicionarCliente;
     private ListView listViewClientes;
     private AdapterCliente adapter;
+    private GestureDetector gestureDetector;
     private ArrayList<Cliente> listaDinamicaClientes;
     private ArrayList<String> arrayList;
+
+    private static final int LIMITE_SWIPE = 100;
+    private static final int LIMITE_VELOCIDADE = 100;
 
     BancoDadosCliente db = new BancoDadosCliente(this);
 
     public static final int NOVO_CLIENTE = 101;
     public static final int ALTERAR_CLIENTE = 102;
 
+    private int viewCounter = 0;
+
+    GestureDetector.SimpleOnGestureListener gestureListener = new GestureDetector.SimpleOnGestureListener() {
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            Log.i("OnFling","X1 = " + e1.getX() + "X2 = " + e2.getX() + "Velocidade" + velocityX);
+            float diferencaX = e2.getX() - e1.getX();
+            if(Math.abs(diferencaX) > LIMITE_SWIPE && Math.abs(velocityX) > LIMITE_VELOCIDADE){
+                if(diferencaX > 0){
+                    Log.i("MOVIMENTO", "Movimento para a direita");
+                }
+                else{
+                    Log.i("MOVIMENTO", "Movimento para a esquerda");
+                }
+            }
+            return true;
+        }
+    };
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return gestureDetector.onTouchEvent(event);
+    }
+
+    View.OnTouchListener touchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            return gestureDetector.onTouchEvent(event);
+        }
+    };
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_list_clients_zero);
 
         initButtonsHub();
@@ -120,6 +155,7 @@ public class ListClientes extends AppCompatActivity {
 
         List<Cliente> clientes = db.listAllClientes();
         listaDinamicaClientes = new ArrayList<Cliente>();
+        gestureDetector = new GestureDetector(this, gestureListener);
 
         ListView listViewClientesTeste;
 
@@ -133,12 +169,26 @@ public class ListClientes extends AppCompatActivity {
 
         listViewClientes = (ListView) findViewById(R.id.listVClientes);
         listViewClientes.setAdapter(adapter);
+        listViewClientes.setOnTouchListener(touchListener);
 
+        listViewClientes.setClickable(true);
         listViewClientes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                openClienteData(i);
-                Log.d("WTF", "\n \n \n \n \n Entro no primero");
+                //openClienteData(i);
+                viewCounter = viewCounter + 1000;
+                Log.d("WTF", "\n \n \n \n \n Entrou no onItemClick");
+                Log.d("WTF", " " + viewCounter);
+            }
+        });
+
+        listViewClientes.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                viewCounter = viewCounter + 1;
+                Log.d("WTF", "\n \n \n \n \n Entrou no onClick Geral");
+                Log.d("WTF", " " + viewCounter);
+                return false;
             }
         });
 
